625f60ce629810d5cf85a2a32cccb0c9fb577a17
Kevin Kolosiana <22205858@me.uvsq.fr>
2025-11-14 17:25:56 +0100

    Creation du depot Projets du cours qualite dev

---
+++ src/Pi.java
@@ -0,0 +1,15 @@
+/**
+ * Approximates PI using the Monte Carlo method.  Demonstrates
+ * use of Callables, Futures, and thread pools.
+ */
+public class Pi 
+{
+    public static void main(String[] args) throws Exception 
+    {
+	long total=0;
+	// 10 workers, 50000 iterations each
+	total = new Master().doRun(50000, 10);
+	System.out.println("total from Master = " + total);
+    }
+}
+
